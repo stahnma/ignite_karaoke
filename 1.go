93efd55917364719717f6e14d19b57863377af82
Michael Stahnke <stahnma@puppet.com>
2018-08-08 00:11:56 -0500

    Now with templating

--- 1.go
+++ 1.go
@@ -1,15 +1,27 @@
 package main
 
+//TODO
+// * Ensure image isn't a 404
+// * Check if there are any NSFW markers and remove those
+// * Allow support for mulitple subreddits
+// * Allow control/start/stop/pause
+// * Servce in golang and not python
+// * Single binary
+// * CLI arguments or env vars for which subreddits
+
 // jq format .data.children[0].data.preview.images[0].source.url
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/jmoiron/jsonq"
 	"github.com/kr/pretty"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
 	"strings"
+	"text/template"
+	"time"
 )
 
 //const uri = "https://www.reddit.com/r/wtfstockphotos/.json?limit=100"
@@ -50,23 +62,67 @@ func get_the_pics(uri string) []string {
 			continue
 		}
 		//pretty.Println(pic)
-		pics = append(pics, pic+"\n")
+		pics = append(pics, pic)
 	}
 
 	return pics
 }
+func contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+func grab20(pics []string) []string {
+	// We want to randomly grab 20 images out of our set
+	if len(pics) < 20 {
+		return pics
+	}
+	seed := rand.NewSource(time.Now().Unix())
+	r := rand.New(seed)
+	var n int
+	var con []string
+	for i := 0; i < 20; i++ {
+		n = r.Intn(len(pics))
+		if !contains(con, pics[n]) {
+			con = append(con, pics[n])
+		} else {
+			i--
+		}
+
+	}
+
+	return con
+}
 
 func main() {
-	base := "https://www.reddit.com/r/wtfstockphotos"
+	var base string
+	base = "https://www.reddit.com/r/disneyvacation"
+	base = "https://www.reddit.com/r/FunnyStockPics"
 	suffixes := []string{"/.json?limit=100", "/top/.json?limit=100", "/new/.json?limit=100", "/top/.json?sort=top&t=month&limit=100", "/top/.json?sort=top&t=all&limit=100"}
-	//suffixes := []string{"/.json?limit=100"}
+	//	suffixes := []string{"/.json?limit=10"}
 	var pics []string
 	for _, suf := range suffixes {
-		pretty.Println(suf)
+		//pretty.Println(suf)
 		uri := base + suf
-		pretty.Println("URI is " + uri)
+		//pretty.Println("URI is " + uri)
 		pics = append(pics, get_the_pics(uri)...)
 	}
 	pics = dedupe(pics)
-	fmt.Println(len(pics))
+	//fmt.Println(len(pics))
+
+	the20 := grab20(pics)
+
+	tmpl := template.New("test")
+	tmpl, err := tmpl.ParseFiles("page.tmpl")
+	if err != nil {
+		pretty.Println(err)
+	}
+	err = tmpl.ExecuteTemplate(os.Stdout, "page.tmpl", the20)
+	if err != nil {
+		pretty.Println(err)
+	}
 }
